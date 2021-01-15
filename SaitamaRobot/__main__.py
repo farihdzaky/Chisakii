74b485b9f9200405951041fb8e515391fff97536
Zero <69114010+Anomaliii@users.noreply.github.com>
2021-01-16 05:50:13 +0700

    Update __main__.py

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -51,15 +51,25 @@ def get_readable_time(seconds: int) -> str:
 
 
 PM_START_TEXT = """
-Hi {}, my name is {}! 
-I am an Anime themed group management bot.
-Build by weebs for weebs, I specialize in managing anime and similar themed groups.
-You can find my list of available commands with /help.
+Hey there! my name is *{}*.
+Any questions on how to use me? use /help
+
+Join Our [Group](https://t.me/ZeroBotSupport) If You wanna Report Issue 🙂
+
+I'm here to make your group management fun and easy!
+I have lots of handy features ☺️ such as :
+
+• flood control.              • Note'S keeping system.
+• Warning System.   • Predetermined Filters.
+
+*Managed With ❤️ By :* [Zero](https://t.me/Anomaliii)
+
+Wanna Add me to your Group? Just click the button below!
 """
 
 HELP_STRINGS = """
 Hey there! My name is *{}*.
-I'm a Hero For Fun and help admins manage their groups with One Punch! Have a look at the following for an idea of some of \
+I'm an Anime Management Bot to help admins manage their groups! Have a look at the following for an idea of some of \
 the things I can help you with.
 
 *Main* commands available:
@@ -77,10 +87,10 @@ And the following:
     dispatcher.bot.first_name, ""
     if not ALLOW_EXCL else "\nAll commands can either be used with / or !.\n")
 
-SAITAMA_IMG = "https://telegra.ph/file/46e6d9dfcb3eb9eae95d9.jpg"
+SAITAMA_IMG = "https://telegra.ph/file/55b328ec75b76e139745c.jpg"
 
 DONATE_STRING = """Heya, glad to hear you want to donate!
-Saitama is hosted on one of Kaizoku's Servers and doesn't require any donations as of now but \
+Kai is hosted on one of Own's Servers and doesn't require any donations as of now but \
 You can donate to the original writer of the Base code, Paul
 There are two ways of supporting him; [PayPal](paypal.me/PaulSonOfLars), or [Monzo](monzo.me/paulnionvestergaardlarsen)."""
 
@@ -200,27 +210,20 @@ def start(update: Update, context: CallbackContext):
                 reply_markup=InlineKeyboardMarkup(
                     [[
                         InlineKeyboardButton(
-                            text="☑️ Add Saitama to your group",
+                            text="Add to your group 👥",
                             url="t.me/{}?startgroup=true".format(
-                                context.bot.username))
+                                context.bot.username)),
+                        InlineKeyboardButton(
+                             text="Support Group 🎗️",
+                             url=f"https://t.me/{SUPPORT_CHAT}")
                     ],
                      [
                          InlineKeyboardButton(
-                             text="🚑 Support Group",
-                             url=f"https://t.me/{SUPPORT_CHAT}"),
+                             text="Getting started guide 🧾",
+                             url="https://t.me/OnePunchUpdates/29"),
                          InlineKeyboardButton(
-                             text="🔔 Updates Channel",
-                             url="https://t.me/OnePunchUpdates")
-                     ],
-                     [
-                         InlineKeyboardButton(
-                             text="🧾 Getting started guide",
-                             url="https://t.me/OnePunchUpdates/29")
-                     ],
-                     [
-                         InlineKeyboardButton(
-                             text="🗄 Source code",
-                             url="https://github.com/AnimeKaizoku/SaitamaRobot")
+                             text="Source code 🗄",
+                             url="https://github.com/Anomaliii/KaiRobot")
                      ]]))
     else:
         update.effective_message.reply_text(
