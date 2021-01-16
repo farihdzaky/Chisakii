b798649a48b9f09c1a5f60c8736b3072e458769d
Zero <69114010+Anomaliii@users.noreply.github.com>
2021-01-16 12:14:20 +0700

    Update handlers.py

--- SaitamaRobot/modules/helper_funcs/handlers.py
+++ SaitamaRobot/modules/helper_funcs/handlers.py
@@ -1,5 +1,5 @@
-import lynda.modules.sql.blacklistusers_sql as sql
-from lynda import ALLOW_EXCL
+import SaitamaRobot.modules.sql.blacklistusers_sql as sql
+from SaitamaRobot import ALLOW_EXCL
 from telegram import MessageEntity, Update
 from telegram.ext import CommandHandler, MessageHandler, RegexHandler, Filters
 from time import sleep
