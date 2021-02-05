9d0822d732ce5409958cd77e4501c382fda336ea
Zero <69114010+Anomaliii@users.noreply.github.com>
2021-02-05 16:12:42 +0700

    Update ping.py

--- SaitamaRobot/modules/ping.py
+++ SaitamaRobot/modules/ping.py
@@ -80,10 +80,8 @@ def ping(update: Update, context: CallbackContext):
     uptime = get_readable_time((time.time() - StartTime))
 
     message.edit_text(
-        "PONG!!\n"
-        "<b>Time Taken:</b> <code>{}</code>\n"
-        "<b>Service uptime:</b> <code>{}</code>".format(telegram_ping, uptime),
-        parse_mode=ParseMode.HTML)
+        "Pong!!!\n"
+        "</b> <code>{}</code>\n"
 
 
 @run_async
