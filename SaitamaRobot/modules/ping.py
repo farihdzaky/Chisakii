0c800b350562c293eef10d5e932b2b78dd05a3d3
Zero <69114010+Anomaliii@users.noreply.github.com>
2021-02-05 17:10:43 +0700

    Update ping.py

--- SaitamaRobot/modules/ping.py
+++ SaitamaRobot/modules/ping.py
@@ -81,9 +81,7 @@ def ping(update: Update, context: CallbackContext):
 
     message.edit_text(
         "PONG!!\n"
-        "<b>Time Taken:</b> <code>{}</code>\n"
-        "<b>Service uptime:</b> <code>{}</code>".format(telegram_ping, uptime),
-        parse_mode=ParseMode.HTML,
+        "<b></b> <code>{}</code>\n"
     )
 
 
@@ -93,12 +91,10 @@ def pingall(update: Update, context: CallbackContext):
     to_ping = ["Kaizoku", "Kayo", "Telegram", "Jikan"]
     pinged_list = ping_func(to_ping)
     pinged_list.insert(2, "")
-    uptime = get_readable_time((time.time() - StartTime))
 
     reply_msg = "⏱Ping results are:\n"
     reply_msg += "\n".join(pinged_list)
-    reply_msg += "\n<b>Service uptime:</b> <code>{}</code>".format(uptime)
-
+   
     update.effective_message.reply_text(
         reply_msg, parse_mode=ParseMode.HTML, disable_web_page_preview=True
     )
