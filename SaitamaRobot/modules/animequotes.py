d8984c775b06b530a6fd87c663d15a35d9226244
Zero <69114010+Anomaliii@users.noreply.github.com>
2021-02-05 18:14:29 +0700

    Update animequotes.py

--- SaitamaRobot/modules/animequotes.py
+++ SaitamaRobot/modules/animequotes.py
@@ -21,7 +21,7 @@ def animequotes(update: Update, context: CallbackContext):
         random.choice(animequotes_strings.QUOTES_IMG))
 
 __help__ = """
- • `/aq`*:* gives random anime quotes
+ • `/animequotes`*:* gives random anime quotes
  
 """
 ANIMEQUOTES_HANDLER = DisableAbleCommandHandler("animequotes", animequotes)
