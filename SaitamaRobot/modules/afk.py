4fe5322dd7b9259de598bb1f41d5a6e9f460b5f4
farihdzaky <79653902+farihdzaky@users.noreply.github.com>
2021-03-06 20:02:20 +0700

    Update afk.py

--- SaitamaRobot/modules/afk.py
+++ SaitamaRobot/modules/afk.py
@@ -57,9 +57,9 @@ def no_longer_afk(update: Update, context: CallbackContext):
         firstname = update.effective_user.first_name
         try:
             options = [
-                '{} is here!', '{} is back!', '{} is now in the chat!',
-                '{} is awake!', '{} is back online!', '{} is finally here!',
-                'Welcome back! {}', 'Where is {}?\nIn the chat!'
+                ' Welcome Back Baka!', '{} Go Slash the Titans', '{} is now in the chat!',
+                '{} yo', '{} Go find the titan', '{} is finally here!',
+                'Yo {} A Titan', 'Where is {}?\nIn the chat!'
             ]
             chosen_option = random.choice(options)
             update.effective_message.reply_text(chosen_option.format(firstname))
